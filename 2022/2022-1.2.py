48a9329034648369a8aa8e94f190434b3d88f1f8
Alex <alt1999@live.fr>
2023-11-22 23:27:45 -0800

    2022 1.2

--- 2022/2022-1.2.py
+++ 2022/2022-1.2.py
@@ -1,6 +1,8 @@
 with open("numbers.txt", "r") as file:
     numbers = [line.strip() for line in file]
 maxSum=0
+secondSum=0
+thirdSum=0
 sum=0
 i=0
 while i<len(numbers):
@@ -9,8 +11,15 @@ while i<len(numbers):
         i+=1
     else:
         if(sum>maxSum):
+            thirdSum=secondSum
+            secondSum=maxSum
             maxSum=sum
+        elif(sum>secondSum):
+            thirdSum=secondSum
+            secondSum=sum
+        elif(sum>thirdSum):
+            thirdSum=sum         
         sum=0
         i+=1
-        
-print(maxSum)
+     
+print(maxSum+secondSum+thirdSum)
